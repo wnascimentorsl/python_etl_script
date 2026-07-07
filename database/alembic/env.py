f8e05ac41304b88391ce29652be3e064a8ed1864
williansoeltech <willian.nascimento@soeltech.com.br>
2026-07-07 16:27:37 -0300

    fix: changed the env.py so there is no need to have alembic.ini commited, now the path will get the DATABASE URL from env

--- database/alembic/env.py
+++ database/alembic/env.py
@@ -1,13 +1,25 @@
+import os
 from logging.config import fileConfig
-
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from pathlib import Path
 
 from alembic import context
+from dotenv import load_dotenv
+from sqlalchemy import engine_from_config, pool
+
+# This assumes your directory structure looks like:
+# project_root/
+# ├── .env
+# └── migrations/
+#     └── env.py
+
+load_dotenv(Path(__file__).resolve().parent.parent.parent / ".env")
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+if os.getenv("DATABASE_URL"):
+    config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
